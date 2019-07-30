5013132d15dfa62f5cee164cf567f6b48171b0bc
paul <pazoulai@gmail.com>
2019-07-31 01:18:57 +0200

    adds related name "members" to board models

--- api/boards/models.py
+++ api/boards/models.py
@@ -19,7 +19,8 @@ class Member(models.Model):
                              on_delete=models.CASCADE)
     name = models.CharField(max_length=20)
     last_reset = models.DateTimeField()
-    board = models.ForeignKey('boards.Board', on_delete=models.CASCADE)
+    board = models.ForeignKey(
+        'boards.Board', related_name="members", on_delete=models.CASCADE)
 
 
 class Board(models.Model):
