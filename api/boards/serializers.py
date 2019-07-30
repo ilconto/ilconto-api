945d1dd5b25dd244ce5dbc9e2b8e18b5af67051e
paul <pazoulai@gmail.com>
2019-07-31 01:20:28 +0200

    adds first serializers

--- api/boards/serializers.py
+++ api/boards/serializers.py
@@ -1,8 +1,27 @@
 from rest_framework import serializers
-from .models import Board
+from .models import Board, User, Member
 
 
-class BoardSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'id')
+
+
+class MemberSerializer(serializers.Serializer):
+    user = UserSerializer()
+    last_reset = serializers.DateTimeField()
+
+    class Meta:
+        model = Member
+        fields = ("user", "last_reset")
+
+
+class BoardSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=200)
+    id = serializers.IntegerField()
+    members = MemberSerializer(many=True, read_only=True)
+
     class Meta:
         model = Board
-        fields = ("id", "title")
+        fields = ['title', 'id', "members"]
