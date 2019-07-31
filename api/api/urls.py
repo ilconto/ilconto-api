7b063b73597985110d892376490e9c960d80134f
paul <pazoulai@gmail.com>
2019-08-01 00:45:24 +0200

    adds url to get specific board based on pk

--- api/api/urls.py
+++ api/api/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from boards.views import BoardList
+from boards.views import BoardView
 from boards.models import Board
 from rest_framework import routers
 
 urlpatterns = [
-    path("boards/", BoardList.as_view()),
+    path("boards/<pk>", BoardView.as_view()),
     path('admin/', admin.site.urls),
+
 ]
