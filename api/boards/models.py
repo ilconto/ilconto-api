57fd8cbc4d212ab41f5b3041d7def0d7ebc37d85
paul <pazoulai@gmail.com>
2019-08-04 20:43:47 +0200

    adds HStoreField to boards

--- api/boards/models.py
+++ api/boards/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
+from django.contrib.postgres.fields import HStoreField
 
 
 class User(AbstractUser):
@@ -31,6 +32,7 @@ class Board(models.Model):
     Model representing a board
     """
     title = models.CharField(max_length=20)
+    scores = HStoreField()
 
     def __str__(self):
         return self.title
