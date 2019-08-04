bce23a90b78925deddcca19da4303ecfe18c0e05
paul <pazoulai@gmail.com>
2019-08-04 20:33:28 +0200

    adds UserProfile view

--- api/boards/views.py
+++ api/boards/views.py
@@ -1,13 +1,24 @@
-# from rest_framework.views import APIView, RetrieveUpdateDestroyAPIView
+from rest_framework.views import APIView
 from rest_framework import generics, permissions
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAuthenticated
 
-from .models import Board
-from .serializers import BoardSerializer
-from .permissions import IsBoardMember
+
+from .models import Board, User
+from .serializers import BoardSerializer, UserSerializer
+from .permissions import IsBoardMember, IsCurrentUser
 
 
 class BoardDetails(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsBoardMember,)
     queryset = Board.objects.all()
     serializer_class = BoardSerializer
+
+
+class UserProfile(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request, format=None):
+
+        return Response(UserSerializer(request.user).data)
