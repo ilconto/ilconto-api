115abc0ef322591cafc9f3b3a6a85117b4e246e1
paul <pazoulai@gmail.com>
2019-08-04 12:31:00 +0200

    uses drf standard to implement BoardDetails view

--- api/boards/views.py
+++ api/boards/views.py
@@ -1,19 +1,13 @@
-from rest_framework.views import APIView
-from .models import Board
-from .serializers import BoardSerializer
+# from rest_framework.views import APIView, RetrieveUpdateDestroyAPIView
+from rest_framework import generics, permissions
 from rest_framework.response import Response
 
-
-class BoardView(APIView):
-
-    def get(self, request, pk):
-        board = Board.objects.get(pk=pk)
-        serializer = BoardSerializer(board)
-        response = Response(serializer.data)
-        return response
+from .models import Board
+from .serializers import BoardSerializer
+from .permissions import IsBoardMember
 
 
-class ProfileView(APIView):
-
-    def get(self, request):
-        pass
+class BoardDetails(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsBoardMember,)
+    queryset = Board.objects.all()
+    serializer_class = BoardSerializer
