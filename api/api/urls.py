50689bc62e51d0e1e38c05d86e0e6ac0f8cb3299
paul <pazoulai@gmail.com>
2019-08-04 12:31:08 +0200

    adds auth url

--- api/api/urls.py
+++ api/api/urls.py
@@ -6,5 +6,6 @@ from rest_framework_jwt.views import obtain_jwt_token
 urlpatterns = [
     path("api/v1/", include('boards.urls')),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('auth/', obtain_jwt_token)
 ]
