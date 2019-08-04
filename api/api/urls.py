3190b7787ef2f0aecaa6139ee117412aeb3f1174
paul <pazoulai@gmail.com>
2019-08-04 20:27:05 +0200

    removes useless code

--- api/api/urls.py
+++ api/api/urls.py
@@ -1,12 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
-from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     path("api/v1/", include('boards.urls')),
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
     path('api/v1/rest-auth/', include('rest_auth.urls')),
     path('api/v1/rest-auth/registration/',
          include('rest_auth.registration.urls')),
