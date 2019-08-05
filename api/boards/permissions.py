82361caf4585e2d702d4b53f38b71f73a9365679
paul <pazoulai@gmail.com>
2019-08-05 19:50:33 +0200

    updates permission to comply with new user model

--- api/boards/permissions.py
+++ api/boards/permissions.py
@@ -4,8 +4,7 @@ from rest_framework import permissions
 class IsBoardMember(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
-
-        return request.user in [member.user for member in obj.members.all()]
+        return request.user in obj.members.all()
 
 
 class IsCurrentUser(permissions.BasePermission):
