e46f26277ed18dfc2bb93c39e41ff6f4124fecf3
Maxence Lecanu <lecanu.maxence@gmail.com>
2019-08-16 17:56:18 +0200

    feature for board members management v0

--- boards_project/boards/serializers.py
+++ boards_project/boards/serializers.py
@@ -6,7 +6,6 @@ from .models import Board, User
 
 
 class UserSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(read_only=True)
     username = serializers.CharField(max_length=256, required=False)
     email = serializers.EmailField(max_length=256, required=True)
 
@@ -17,33 +16,34 @@ class UserSerializer(serializers.ModelSerializer):
         read_only_fields = ('id', 'boards', 'email',)
 
 
-class UserCompactSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(read_only=True)
-    username = serializers.CharField(max_length=256, required=False)
-    email = serializers.EmailField(max_length=256, required=True)
+class MemberSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(max_length=256, read_only=True)
+    email = serializers.EmailField(max_length=256, read_only=True)
 
     class Meta:
         model = User
         fields = ('username', 'email', 'id',)
-        read_only_fields = ('id', 'email',)
 
 
 class BoardSerializer(serializers.ModelSerializer):
     scores = serializers.JSONField(required=False)
-    members = UserCompactSerializer(many=True, required=False)
+    members = MemberSerializer(many=True, required=False)
 
     class Meta:
         model = Board
         fields = ('id', 'title', 'scores', 'members',)
-        read_only_fields = ('id',)
+        read_only_fields = ('id', 'members', 'scores')
+        depth = 1
 
     def create(self, validated_data):
         """
         This function is triggered during access to the create view
         Classic attributes are set as usual (basically this is just the board's title)
         For members and scores:
+
         - The requesting user is added to the baord members by default
         !!! IMPORTANT !!! : This should be mentionned as a warning in the front applciation
+
         - The dates in the "scores" attribute of validated_data are used for members last reset
         initialization if provided. Otherwise the last reset time of a member is set to
         datetime.datetime.now().
@@ -82,51 +82,12 @@ class BoardSerializer(serializers.ModelSerializer):
 
         return board
 
-    def update(self, instance, validated_data):
-        """
-        This function is triggered during access to the update view.
-        Behaviours:
-        - If there is only classic attributes that are updated -> the update is performed as usual
-        - If the "members" attribute is different:
-            - New members are added to the board
-            - Deleted members are deleted from the board
-        - For each members in the new "members" attribute:
-            - If no last reset is provided -> the last_reset is set to the last value in the scores infos,
-            or it is set to datetime.datetime.now() if it is a new member.
-            - Otherwise, the value of last_reset is set to the value contained in the scores attributes if and
-            only if the user requesting a modification matches the member email.
-        
-        Additional possible feature: Add a 403 response when trying to modify the score of another member
-        """
 
-        # Basic attribute modification
-        instance.title = validated_data.get('title', instance.title)
-        instance.save()
 
-        # Members management
-        members_data = validated_data.get('members', instance.members.all())
-        scores_data = validated_data.get('scores', {})
+class BoardPartialSerializer(serializers.ModelSerializer):
+    title = serializers.CharField(required=False)
 
-        for member in members_data:
-            # Eventually update if member exists
-            email = member['email']
-            if instance.members.filter(email=email).exists():
-                instance.reset_score(
-                    email, 
-                    scores_data.get(
-                        email,
-                        instance.scores[email]
-                    )
-                )
-            # If user is not a member of the board yet, perform the add or register routine
-            else:
-                try:
-                    user = User.objects.get(email=email)
-                    instance.add_member(user)
-                    instance.reset_score(email, scores_data.get(email, datetime.now()))
-                except User.DoesNotExist:
-                    # That's where we'll implement the email to non-existing members feature
-                    # @TODO: Implement it !
-                    pass
-
-        return instance
+    class Meta:
+        model = Board
+        fields = ('id', 'title')
+        read_only_fields = ('id',)
--- boards_project/boards/views.py
+++ boards_project/boards/views.py
@@ -1,14 +1,28 @@
+from datetime import datetime
+
+
 from rest_framework.views import APIView
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 
+from django.shortcuts import get_object_or_404
+
+
 from .models import Board, User
-from .serializers import BoardSerializer, UserSerializer
+from .serializers import (
+    BoardSerializer,
+    BoardPartialSerializer,
+    UserSerializer,
+    MemberSerializer,
+)
 from .permissions import IsBoardMember, IsCurrentUser
 
 
+""" ===============================
+============ Board views ==========
+=============================== """
 class ListCreateBoardsView(generics.ListCreateAPIView):
     """
     This view extends the generic ListCreateAPIView, overriding it's get_queryset method
@@ -18,6 +32,7 @@ class ListCreateBoardsView(generics.ListCreateAPIView):
     """
     permission_classes = (IsAuthenticated,)
     serializer_class = BoardSerializer
+
     def get_queryset(self):
         if self.request.user.is_staff:
             return Board.objects.all()
@@ -25,10 +40,103 @@ class ListCreateBoardsView(generics.ListCreateAPIView):
             return self.request.user.boards.all()
 
 
-class BoardDetailsView(generics.RetrieveUpdateDestroyAPIView):
+class  RetrieveUpdateDeleteBoardsView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    This view deals with retrieving a board and deleting it, as well as basic
+    updates (title). For updating board members, a custom view is used.
+    """
     permission_classes = (IsBoardMember,)
     queryset = Board.objects.all()
-    serializer_class = BoardSerializer
+    
+    def get_serializer_class(self):
+        if self.request.method in ('PUT',):
+            return BoardPartialSerializer
+        else:
+            return BoardSerializer
+
+
+class ListCreateBoardMembersView(generics.ListCreateAPIView):
+    """
+    This views deals with adding and listing board members
+    """
+
+    permission_classes = (IsBoardMember,)
+    serializer_class = MemberSerializer
+    
+    def get_queryset(self):
+        board_id = self.kwargs['board_id']
+        board = get_object_or_404(Board.objects.all(), id=board_id)
+        return board.members.all()
+
+
+    def create(self, request, board_id):
+        board = get_object_or_404(Board.objects.all(), id=board_id)
+        board_members = board.members.all()
+
+        serialized_user = UserSerializer(data=request.data['user'])
+        score = request.data.get('scores', datetime.now())
+
+        if serialized_user.is_valid():
+            new_member = serialized_user.validated_data
+            try:
+                email = new_member['email']
+                user = User.objects.get(email=email)
+                board.add_member(user)
+                board.reset_score(email, score)
+            except User.DoesNotExist:
+                # That's where we'll implement the email to non-existing members feature
+                # @TODO: Implement it !
+                pass
+        else:
+            pass
+
+        return Response({
+            "id": user.id,
+            "email": user.email,
+            "score": score
+        })
+
+
+class RetrieveUpdateDeleteBoardMembersView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    This view deals with retrieving a member infos, updating it's score, or deleting it
+    """
+    permission_classes = (IsBoardMember,)
+    serializer_class = UserSerializer
+    
+    def retrieve(self, request, board_id, member_id):
+        board = get_object_or_404(Board.objects.all(), id=board_id)
+        board_member = get_object_or_404(board.members.all(), id=member_id)
+        
+        return Response({
+            "member_details": MemberSerializer(board_member).data,
+            "score": board.scores[board_member.email]
+        })
+
+    def update(self, request, board_id, member_id):
+        board = get_object_or_404(Board.objects.all(), id=board_id)
+        board_member = get_object_or_404(board.members.all(), id=member_id)
+
+        ts = request.data.get('timestamp', datetime.timestamp(datetime.now()))
+        score = datetime.fromtimestamp(ts)
+        board.reset_score(board_member, score)
+
+        return Response({
+            "id": board_member.id,
+            "email": board_member.email,
+            "score": score
+        })
+
+    def delete(self, request, board_id, member_id):
+        board = get_object_or_404(Board.objects.all(), id=board_id)
+        board_member = get_object_or_404(board.members.all(), id=member_id)
+
+        board.remove_member(board_member)
+
+        return Response({
+            "id": board_member.id
+        })
+
 
 
 class UserProfileView(APIView):
