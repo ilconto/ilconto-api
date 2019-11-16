a0fb246eedd60a1d485750b262e596877e74fab5
paul <pazoulai@gmail.com>
2019-11-17 00:13:13 +0100

    adds comment

--- boards_project/boards/models.py
+++ boards_project/boards/models.py
@@ -31,7 +31,7 @@ class AppUser(AbstractUser):
 class BoardMember(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     username = models.CharField(max_length=32)
-    score = models.IntegerField()
+    score = models.IntegerField()    # score = last time in UTC seconds
     user = models.ForeignKey('boards.AppUser', on_delete=models.CASCADE, related_name='memberships')
     board = models.ForeignKey('boards.Board', on_delete=models.CASCADE, related_name='members')
     created_at = models.DateTimeField(auto_now_add=True)
