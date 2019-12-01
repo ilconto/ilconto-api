ed5ccee1063cbaccc358d1e64c03b6e0de82b2e8
Maxence Lecanu <lecanu.maxence@gmail.com>
2019-12-01 12:06:29 +0100

    assets collection

--- boards_project/api/settings.py
+++ boards_project/api/settings.py
@@ -168,7 +168,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = './assets'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
